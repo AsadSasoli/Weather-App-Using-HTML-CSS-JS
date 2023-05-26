eaf33ddbb6a92aee9347996467059d7f151586c6
AsadSasoli <asadsasoli8@gmail.com>
2023-05-26 06:21:40 +0500

    2nd

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function bttn() {
         console.log(datt)
         document.getElementById('ccity').innerHTML=datt.location.city
         document.getElementById('ctxt').innerHTML=datt.current_observation.condition.text
-        document.getElementById('ctem').innerHTML=datt.current_observation.condition.temperature+"C"
+        document.getElementById('ctem').innerHTML=datt.current_observation.condition.temperature+"°C"
         var weather = datt.current_observation.condition.text
         if (weather == "Partly Cloudy") {
             document.getElementById('cimag').src="IMG/Partly Cloudy.png"
@@ -25,15 +25,67 @@ function bttn() {
             document.getElementById('cimag').src="IMG/Mostly Cloudy.png"
             document.getElementById('cimag').style.width="100px"
             document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Cloudy"){
+            document.getElementById('cimag').src="IMG/Partly Cloudy.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Haze"){
+            document.getElementById('cimag').src="IMG/Haze.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Thunderstorms"){
+            document.getElementById('cimag').src="IMG/Scattered Thunderstorms.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Rainy"){
+            document.getElementById('cimag').src="IMG/Rain Cloud Storm.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Sunny"  ||weather == "Hot"){
+            document.getElementById('cimag').src="IMG/Sunny.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Smoke"){
+            document.getElementById('cimag').src="IMG/Smoke.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
+        }else if(weather == "Clear"){
+            document.getElementById('cimag').src="IMG/CLear.png"
+            document.getElementById('cimag').style.width="100px"
+            document.getElementById('weather-card').style.width="300px"
         }
+        var fimage
         for(var i=1;i<=6;i++){
+            if(datt.forecasts[i].text == "Partly Cloudy"){
+                fimage = "IMG/Partly Cloudy.png"
+            }else if(datt.forecasts[i].text == "Mostly Cloudy"){
+                fimage = "IMG/Mostly Cloudy.png"
+            }else if(datt.forecasts[i].text == "Cloudy"){
+                fimage = "IMG/Partly Cloudy.png"
+            }else if(datt.forecasts[i].text == "Haze"){
+                fimage = "IMG/Haze.png"
+            }else if(datt.forecasts[i].text == "Thunderstorms"){
+                fimage = "IMG/Scattered Thunderstorms.png"
+            }else if(datt.forecasts[i].text == "Rainy"){
+                fimage = "IMG/Rain Cloud Storm.png"
+            }else if(datt.forecasts[i].text == "Sunny"){
+                fimage = "IMG/Sunny.png"
+            }else if(datt.forecasts[i].text == "Smoke"){
+                fimage = "IMG/Mostly IMG/Smoke.png"
+            }else if(datt.forecasts[i].text == "Clear"){
+                fimage = "IMG/CLear.png"
+            }else if(datt.forecasts[i].text == "Mostly Sunny"){
+                fimage = "IMG/Sunny.png"
+            }else if(datt.forecasts[i].text == "Hot"){
+                fimage = "IMG/Sunny.png"
+            }
             document.getElementById('data').innerHTML+=`
                 <div class="card">
                     <h1>${datt.forecasts[i].day}</h1>
                     <h3>${datt.forecasts[i].text}</h3>
-                    <h2>Hight:<span>${datt.forecasts[i].high}</span></h2>
-                    <h2>Low:<span>${datt.forecasts[i].low}</span></h2>
-                    <img src="IMG/Partly Cloudy.png" width="100px" height="100px">
+                    <h2>Hight:<span>${datt.forecasts[i].high}°C</span></h2>
+                    <h2>Low:<span>${datt.forecasts[i].low}°C</span></h2>
+                    <img src="${fimage}" width="100px" height="100px">
                 </div>
             `
         }
